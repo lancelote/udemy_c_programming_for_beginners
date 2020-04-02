e49e7f62881bc79251a85e5b9aec1c1c61a0d2ea
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-04-02 20:52:30 +0300

    Use C99 bool

--- src/section5/basic_types.c
+++ src/section5/basic_types.c
@@ -1,8 +1,10 @@
+#include <stdbool.h>
+
 int main(void) {
     int integerVar = 100;
     float floatingVar = 333.79;
     double doubleVar = 8.44e+11;
-    _Bool boolVar = 1;
+    bool boolVar = true;
 
     return 0;
 }
